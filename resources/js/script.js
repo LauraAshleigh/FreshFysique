c330e357ea026eda35f978b2991aa180f3020a81
Laura <39982146+LauraLettoofCronin@users.noreply.github.com>
2019-10-22 19:06:37 +1100

    Update design

--- resources/js/script.js
+++ resources/js/script.js
@@ -18,37 +18,7 @@ $(document).ready(function(){
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--mind-body-fitness').offset().top}, 1000);
     });
-    
-    /* Animations on scroll */
-    $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '60%;'
-    });    
-    
-    $('.js--wp-5').waypoint(function(direction) {
-        $('.js--wp-5').addClass('animated fadeInUp');
-    }, {
-        offset: '60%;'
-    });
+
 
     /* Mobile navigation */
     $('.js--nav-icon').click(function(){
@@ -57,12 +27,12 @@ $(document).ready(function(){
         
         nav.slideToggle(200);
         
-        if(icon.hasClass('ion-md-menu')){
-            icon.addClass('ion-md-close');
-            icon.removeClass('ion-md-menu');
+        if(icon.hasClass('fa-bars')){
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
         } else {
-            icon.addClass('ion-md-menu');
-            icon.removeClass('ion-md-close');
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
         }
     });
     
